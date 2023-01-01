cc19c2fd8e6c9f31493214d094c8dcac99bb4efc
Ayush0049 <121080345+Ayush0049@users.noreply.github.com>
2023-01-01 15:08:59 +0530

    Add files via upload

---
+++ ascii.c
@@ -0,0 +1,13 @@
+//Ascii value of the character
+
+#include <stdio.h>
+int main() {  
+    char c;
+    printf("Enter a character: ");
+    scanf("%c", &c);  
+    
+
+    printf("ASCII value = %d", c);//ascii value of upper &lower case character are different like a=97,A=65
+    
+    return 0;
+}
